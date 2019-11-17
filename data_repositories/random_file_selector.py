1990721a1ae4536f2df8db817c9c16308e72f5b9
VWesp <valentinwesp@yahoo.de>
2019-11-17 14:36:35 +0100

    updated random selector

--- data_repositories/random_file_selector.py
+++ data_repositories/random_file_selector.py
@@ -5,22 +5,20 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description="Choose a random number of individual files from a data repository")
-parser.add_argument("-dp", "--dataportal", help="Select a dataportal (options: dryad, gbif, pangaea, zenodo, figshare)", required=True)
-parser.add_argument("-mf", "--metadataformat", help="Select a format (options: oai_dc, eml, pan_md, datacite)", required=True)
+parser.add_argument("-fs", "--files", help="Set the path to the directory with the XML files", required=True)
 parser.add_argument("-p", "--population", help="Set the number of files that will be selected", type=int, required=True)
 parser.add_argument("-s", "--seed", help="Set the seed to obtain previous results", default=None)
 args = parser.parse_args()
 
 try:
-    data_path = args.dataportal + "/" + args.metadataformat
     file_list = []
-    for dirpath, dirnames, filenames in os.walk(data_path):
+    for dirpath, dirnames, filenames in os.walk(args.files):
         for file in filenames:
             if(file.endswith(".xml")):
                 file_list.append(file)
 
     if(not len(file_list)):
-        raise Exception("No XML file found in path '" + data_path + "'.")
+        raise Exception("No XML file found in path '" + args.files + "'.")
 
     if(args.population > len(file_list)):
         raise Exception("The population size cannot be larger than the number of files.")
@@ -30,14 +28,14 @@ try:
 
     random.seed(args.seed)
     selected_files = random.sample(file_list, args.population)
-    if(not os.path.exists(data_path + "/selected_files")):
-        os.mkdir(data_path + "/selected_files")
+    if(not os.path.exists(args.files + "/selected_files")):
+        os.mkdir(args.files + "/selected_files")
 
     for file in selected_files:
         print(file)
-        shutil.copyfile(data_path + "/" + file, data_path + "/selected_files/" + file)
+        shutil.copyfile(args.files + "/" + file, args.files + "/selected_files/" + file)
 
-    with open(data_path + "/selected_files/seed.txt", "w") as seedWriter:
+    with open(args.files + "/selected_files/seed.txt", "w") as seedWriter:
         print("Seed: " + args.seed)
         seedWriter.write(str(args.seed))
 except Exception as ex:
